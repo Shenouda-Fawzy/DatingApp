bdfee73847643911f37e616bb197b65b93f86684
Shenouda Fawzy <Shenouda.Fawzy@hotmail.com>
2020-01-19 02:15:49 +0200

    End of section 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +13,16 @@ export class RegisterComponent implements OnInit {
 
   @Output() canelRegister = new EventEmitter();
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('registeration successfull');
-    }, error => {
-      console.log('Error in registeration component');
-      console.log(error);
+      this.alertify.success('Registiratoin successfull')
+      }, error => {
+      this.alertify.error(error);
     });
     console.log(this.model);
   }
